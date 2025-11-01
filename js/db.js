b807530e695f3869b923d0f48d9ca0bbf78e64de
nickyc-sketch <nicky_c@live.be>
2025-11-02 00:09:22 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,6 +1,6 @@
 // /js/db.js
-window.VITE_SUPABASE_URL  = "https://xdmrikvxyeebeusnbzav.supabase.co";
-window.VITE_SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXJpa3Z4eWVlYmV1c25iemF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Mjc5MjgsImV4cCI6MjA3NzQwMzkyOH0.WmOMDZoFSk9RwQE1lT9yUuOCwsMjZbKVQrtNpBflpB0";
+window.SUPABASE_URL  = "https://xdmrikvxyeebeusnbzav.supabase.co";
+window.SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXJpa3Z4eWVlYmV1c25iemF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Mjc5MjgsImV4cCI6MjA3NzQwMzkyOH0.WmOMDZoFSk9RwQE1lT9yUuOCwsMjZbKVQrtNpBflpB0";
 
 if (typeof supabase !== "undefined") {
   window.supabase = supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON);
