286e81d863ef9504466c8172a77aeeec7b062aca
nickyc-sketch <nicky_c@live.be>
2025-11-12 21:50:02 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
-<script>
-  window.VRIJEPLEK = {
-    SUPABASE_URL: "https://xdmrikvxyeebeusnbzav.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXJpa3Z4eWVlYmV1c25iemF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Mjc5MjgsImV4cCI6MjA3NzQwMzkyOH0.WmOMDZoFSk9RwQE1lT9yUuOCwsMjZbKVQrtNpBflpB0"
-  };
-</script>
+window.VRIJEPLEK = {
+  SUPABASE_URL: "https://xdmrikvxyeebeusnbzav.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXJpa3Z4eWVlYmV1c25iemF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4Mjc5MjgsImV4cCI6MjA3NzQwMzkyOH0.WmOMDZoFSk9RwQE1lT9yUuOCwsMjZbKVQrtNpBflpB0"
+};
+
